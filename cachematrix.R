d67b348e286669c9cbe3adbbfcc940446dbec8ec
mjosephz <mjzukoski@gmail.com>
2015-05-23 19:58:42 -0400

    Added code & comments for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,17 @@
 
 makeCacheMatrix <- function(x = matrix()) {
      inv <- NULL
+     
      set <- function(y) {
+         ## operator "<<-" assigns value to var in a diff
+         ## environment than the current environment
          x <<- y
          inv <<- NULL
      }
-     get <- function() x
+     get <- function() x 
      setinv <- function(inverse) inv <<- inverse
      getinv <- function() inv
+     ## set a list and then return it to the calling environment
      list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
@@ -22,14 +26,16 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
       inv = x$getinv()
+      ## if is.null = FALSE then cached data exists - return that
       if (!is.null(inv)) {
           message("Getting cached data")
           return(inv)
       }
-      
+      ## else have to solve for the inverse
       calcinv = x$get()
       inv = solve(calcinv, ...)
     
+      ## set the value of the inverse and then return it
       x$setinv(inv)
       return(inv)
 }
